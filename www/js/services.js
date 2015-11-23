a6088962af27e5770c99225ad55f9eb3bb1c66b7
Neegbeah Reeves <Nehemiahsal@aol.com>
2015-11-23 17:29:07 -0500

    Background color changed

--- www/js/services.js
+++ www/js/services.js
@@ -28,6 +28,33 @@ angular.module('GSOData.services',[])
                 }
             });
         },
+        create:function(data){	        
+            return $http.post('https://api.parse.com/1/classes/Food',data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        edit:function(id,data){
+            return $http.put('https://api.parse.com/1/classes/Food/'+id,data,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        },
+        delete:function(id){
+            return $http.delete('https://api.parse.com/1/classes/Food/'+id,{
+                headers:{
+                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
+                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
+                    'Content-Type':'application/json'
+                }
+            });
+        }
        
     }
     
